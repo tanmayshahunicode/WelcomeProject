2656f6befaa36d9f5954f0b8a071a4d4b587fcc3
Admin <Admin@DESKTOP-GS0CP1T>
2023-08-12 19:35:08 +0530

    New test case is created and modified existing test case.

--- src/gitGitHub/GITGITHubTesting.java
+++ src/gitGitHub/GITGITHubTesting.java
@@ -5,6 +5,7 @@ public class GITGITHubTesting {
 	public static void main(String[] args) {
 
 		System.out.println("Hello..This is GIT And GITHub");
+		System.out.println("New logic is implented");
 	}
 
 }
